1e4c352151bbf555be0407285088f73b3c5ddc64
mdfaizan973 <faizan.md9735@gmail.com>
2024-08-10 16:06:22 +0530

    added link sectin

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       {/* <PortfolioVersion1 /> */}
-
       <PortfolioVersionTwoContainerStyled>
         <PortfolioVersion2 />
       </PortfolioVersionTwoContainerStyled>
