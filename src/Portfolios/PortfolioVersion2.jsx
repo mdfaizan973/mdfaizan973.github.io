8ab9bdb4073464c41db3815f6d0c3b2b8ea92af1
mdfaizan973 <faizan.md9735@gmail.com>
2024-11-10 16:55:11 +0530

    new code

--- src/Portfolios/PortfolioVersion2.jsx
+++ src/Portfolios/PortfolioVersion2.jsx
@@ -12,7 +12,6 @@ export default function PortfolioVersion2() {
   return (
     <>
       {/* <AnimatedCursor /> */}
-
       <StickyContainer>
         <TopResumeButton />
       </StickyContainer>
